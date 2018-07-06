cf4f6e993ba5aeee5eb7a0c317a1d92e4137272f
crown0128 <happycrown0128@gmail.com>
2018-07-06 13:33:52 +0300

    added github pages

---
+++ docs/js/script.js
@@ -0,0 +1,14 @@
+const navButton = document.querySelector(".header__button");
+const upButton = document.querySelector(".page-footer__button");
+const downButton = document.querySelector(".page-header__arrow");
+const nav = document.querySelector(".header__navigation");
+const evaluationSection = document.querySelector(".evaluation");
+const pageHeader = document.querySelector(".page-header");
+
+navButton.addEventListener("click", ({target}) => {
+  target.classList.toggle("header__button--open");
+  nav.classList.toggle("header__navigation--open");
+});
+
+upButton.addEventListener("click", () => pageHeader.scrollIntoView());
+downButton.addEventListener("click", () => evaluationSection.scrollIntoView());
